ca946f1a485ee7e22e8219af2259f034cda9b421
Linda <linda.dogan@outlook.fr>
2020-01-17 14:45:26 +0100

    Ajout du formulaire, de l'enregistrement du user et gestion d'erreurs

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -53,6 +55,11 @@ class User implements UserInterface
      * @ORM\Column(type="date")
      */
     private $creationdate;
+
+    public function __construct() {
+        $this->creationdate = new \DateTime();
+    }
+
 
     public function getId(): ?int
     {
